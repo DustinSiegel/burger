b3e1bd0212a85652f7455e65ae714e62e94bb945
Dustin J Siegel <weenzombie@gmail.com>
2017-09-08 20:53:01 -0600

    connections.js is set up with a config.json and started handlebars

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+// Connect to Mysql using Sequelize through a json file.
+var Sequelize = require("sequelize");
+var sequelize;
+var env       = process.env.NODE_ENV || 'development';
+var config    = require(__dirname + '/../config/config.json')[env];
+var db        = {};
+
+if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+
+module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 // Dependencies ============================================================================================================
 var express = require("express");
+var exphbs  = require('express-handlebars');
 var bodyParser = require("body-parser"); 
 var methodOverride = require('method-override');
+var Sequelize = require('sequelize');
 var app = express();	
-var PORT = process.env.PORT || 4000; 
+var PORT = process.env.PORT || 3000; 
 
 // BodyParser types =======================================================================================================
 app.use(bodyParser.json());
@@ -13,10 +15,15 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // override with the X-HTTP-Method-Override header in the request =========================================================
 app.use(methodOverride('_method'));
+app.use(express.static("public"));
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 // Points the server to the route files ===================================================================================
-// require("./app/routing/apiRoutes")(app);
-// require("./app/routing/htmlRoutes")(app);
+var connect = require("./controllers/burgers_controller.js");
+ 
+app.use('/', connect);
 
 // App listener with an alert =============================================================================================
 app.listen(PORT, function() {

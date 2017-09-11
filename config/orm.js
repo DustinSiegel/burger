b07fd59c608754201979c3e4c03682627f1a2f48
Dustin J Siegel <weenzombie@gmail.com>
2017-09-10 18:36:54 -0600

    burgers can now be devoured

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,30 @@
 // Connect to the connection.js file ======================================================================================
 var connection = require("./connection.js");
 
+// creates and array that finds the end of the table data and sets the next set of data there =============================
+function putInData(num) {
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
+}
+
+// Helper function for SQL syntax.
+function sendObject(ob) {
+  var arr = [];
+
+  for (var key in ob) {
+    if (Object.hasOwnProperty.call(ob, key)) {
+      arr.push(key + "=" + ob[key]);
+    }
+  }
+
+  return arr.toString();
+}
+
 // A variable to hold the Object Relational Models ========================================================================
 var orm = {
 	
@@ -23,11 +47,11 @@ var orm = {
 		queryString += columns.toString();
 		queryString += ") ";
 		queryString += "VALUES ";
-		queryString += insertIt;
+		queryString += putInData(insertIt.length);
 		queryString += ") ";
 
 		console.log("queryString", queryString)
-		connection.query(queryString, vals, function(err, result) {
+		connection.query(queryString, insertIt, function(err, result) {
 			if (err) {
 				throw err;
       		};
@@ -36,12 +60,12 @@ var orm = {
 	},
 
 // An ORM that updates one table entry ===================================================================================
-	updateOne: function(burgers, values, change, show) {
+	updateOne: function(burgers, values, condition, show) {
     	var queryString = "UPDATE " + burgers;
 	    queryString += " SET ";
-	    queryString += objToSql(values);
+	    queryString += sendObject(values);
 	    queryString += " WHERE ";
-	    queryString += change;
+	    queryString += condition;
 
 	    console.log("queryString", queryString)
     	connection.query(queryString, function(err, result) {
